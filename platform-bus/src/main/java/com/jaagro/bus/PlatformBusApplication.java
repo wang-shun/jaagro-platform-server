5f4a055de8178e02cc3fff9a269524eba40074cb
zhengxingtao <zheng_xigntao@126.com>
2018-09-23 17:09:49 +0800

    优化pom

---
+++ platform-bus/src/main/java/com/jaagro/bus/PlatformBusApplication.java
@@ -0,0 +1,12 @@
+package com.jaagro.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformBusApplication.class, args);
+    }
+}
---
+++ platform-bus/src/test/java/com/jaagro/bus/PlatformBusApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jaagro.bus;
+
+import com.jaagro.bus.message.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlatformBusApplicationTests {
+    @Autowired
+    private Sender sender;
+
+    @Test
+    public void contextLoads() {
+        sender.sender();
+    }
+
+}
